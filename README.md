94d77839cec580b65f4a50259284a995e652a561
Milan Sriananthan <milan@Milans-MacBook-Pro.local>
2022-01-08 19:17:56 -0500

    Add Readme and clean-up

---
+++ README.md
@@ -0,0 +1,29 @@
+# Nutrition Visualization
+
+Provides a data visualization of the nutrients of any food
+
+## Purpose:
+
+This application was created as a quick way for users to view the nutrition facts of
+a specific food quickly and easily. Here users can quickly note down the nutrition facts
+of the foods they eat and plan their diet accordingly. This application can take in the
+input of almost any food given any quantity.
+
+## Reason
+
+I have always wanted to track my macros and the number of calories I am eating everday,
+however it becomes tedious when I repeatedly have to google search the food I am eating
+and find websites that have the correct data. Which is why I created this application to
+solve this issue. Here on one site I can quickly enter any food I want and right away get
+the correct data in a visual style.
+
+## How it works:
+
+This application was coded purely using JavaScript. It utilized the Spoonacular Food
+API data server to get data on the various foods. To find more information of this API
+see https://spoonacular.com/food-api.
+
+## Screenshots:
+
+<img src="Screenshots/StartingScreen.png" alt="Home Screen"/>
+<img src="Screenshots/FoodSearch.png" alt="Search Apple"/>
--- script.js
+++ script.js
@@ -102,28 +102,7 @@ async function foodid(food, amount) {
   const majorNutrients = notLarge(notZero, maxAmount);
 
   draw(300, 350, majorNutrients);
-
-  // mainForm.insertAdjacentText(
-  //   "afterend",
-  //   `${nutritionfacts[i].name}: ${nutritionfacts[i].amount}${nutritionfacts[i].unit} `
-  // );
-  // if (nutritionfacts[i].title === "Calories") {
-  //   var calories = nutritionfacts[i].amount;
-  //   //   console.log(calories);
-  // }
-
-  //   console.log(typeof calories);
-  //   console.log(calories);
-  //   document.getElementById("cal").textContent = calories;
 }
-
-// async function findFromId(id, amount) {
-//   const response = await fetch(
-//     `https://api.spoonacular.com/food/ingredients/${id}/information?amount=${amount}&unit=grams&apiKey=df150d1925174045a287922c56a2a01c`
-//   );
-//   const data = await response.json();
-//   console.log(data);
-// }
 
 button.addEventListener("click", function (e) {
   e.preventDefault();
@@ -133,21 +112,3 @@ button.addEventListener("click", function (e) {
   const amount = document.getElementById("amount").value;
   foodid(food, amount);
 });
-
-// const canvas = document.getElementById("canvas");
-// const context = canvas.getContext("2d");
-
-// window.addEventListener("resize", resizeCanvas, false);
-
-// function resizeCanvas() {
-//   canvas.width = window.innerWidth;
-//   canvas.height = window.innerHeight;
-
-//   drawStuff();
-// }
-
-// resizeCanvas();
-
-// function drawStuff() {
-//   // do your drawing stuff here
-// }
